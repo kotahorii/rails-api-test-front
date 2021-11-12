377883a3d1b8b3ec17703e6d28d1ec2f2aaf8670
kotahorii <khorii.micron@gmail.com>
2021-11-12 14:34:39 +0900

    initial commit

---
+++ src/components/pages/Test.tsx
@@ -0,0 +1,11 @@
+import { VFC } from 'react'
+
+export const Test: VFC = () => {
+  return (
+    <>
+      <button className="px-3 py-2 bg-blue-700 hover:bg-blue-500 text-white rounded-xl shadow-lg">
+        Submit
+      </button>
+    </>
+  )
+}
